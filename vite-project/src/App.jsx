67668148763596259a02b7d7c949e1414aca527a
thejjwang <jonathanjacobwang@gmail.com>
2023-08-23 11:52:42 -0700

    displayWeather component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import displayWeather from './displayWeather'
 
 function App() {
 
--- vite-project/src/displayWeather.jsx
+++ vite-project/src/displayWeather.jsx
@@ -1,4 +1,4 @@
-function displayWeather(weather) {
+const displayWeather = (weather) => {
     temp.innerHTML = weather.current.temp_f + "&deg;";
     city.innerHTML = weather.location.name;
     wind.innerHTML = weather.current.wind_mph + " MPH";
